eabbf285a5aff0ed2b653da638a92f542749ce76
young <1398508918@qq.com>
2016-10-27 18:30:02 +0800

    增加了淘圈分享功能 by yang

--- app/src/main/java/com/school/twohand/activity/taoquan/EachTaoquanActivity.java
+++ app/src/main/java/com/school/twohand/activity/taoquan/EachTaoquanActivity.java
@@ -44,6 +44,8 @@ import java.util.List;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
  * 单个淘圈的页面
@@ -106,7 +108,6 @@ public class EachTaoquanActivity extends AppCompatActivity {
         init();
         initView();
         initData();
-        Log.i("EachTaoquanActivity", "哈哈啊");
 
     }
 
@@ -451,6 +452,7 @@ public class EachTaoquanActivity extends AppCompatActivity {
                 startActivity(intent1);
                 break;
             case R.id.iv_each_taoquan_share:
+                showShare();
                 break;
             case R.id.LL_1:
                 LL_1.setVisibility(View.GONE);
@@ -547,4 +549,34 @@ public class EachTaoquanActivity extends AppCompatActivity {
             initView();
         }
     }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("来自app:校园二手圈的分享");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://com.baidu.com");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我在淘圈“"+amoyCircle.getCircleName()+"”\n快下载校园二手圈app一起来玩吧");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://pic.4j4j.cn/upload/pic/20130815/31e652fe2d.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://www.baidu.com");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("这款软件不错哦");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://www.baidu.com");
+        // 启动分享GUI
+        oks.show(this);
+    }
+
 }
