f9d77b02ab12137c2beda0b4f2355a90f62a2d8c
young <1398508918@qq.com>
2016-10-27 17:40:19 +0800

    第一次提交 by yang

--- app/src/main/java/com/school/twohand/activity/taoquan/EachTaoquanActivity.java
+++ app/src/main/java/com/school/twohand/activity/taoquan/EachTaoquanActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -105,7 +106,7 @@ public class EachTaoquanActivity extends AppCompatActivity {
         init();
         initView();
         initData();
-
+        Log.i("EachTaoquanActivity", "onCreate: 111");
     }
 
     private void init() {
