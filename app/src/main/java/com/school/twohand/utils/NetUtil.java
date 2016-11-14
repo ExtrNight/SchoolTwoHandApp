8eabb3007c650e5e5cec0440dd6758badab3e29b
young <1398508918@qq.com>
2016-11-14 21:19:38 +0800

    2016-11-10 by yang

--- app/src/main/java/com/school/twohand/utils/NetUtil.java
+++ app/src/main/java/com/school/twohand/utils/NetUtil.java
@@ -4,13 +4,13 @@ package com.school.twohand.utils;
  * Created by yang on 2016/9/30.
  */
 public class NetUtil {
-    public static final String url = "http://10.30.6.45:8080/SchoolTwoHandWeb/";//by yang 高博
+    //public static final String url = "http://10.30.6.45:8080/SchoolTwoHandWeb/";//by yang 高博
     //public static final String url = "http://192.168.191.1:8080/SchoolTwoHandWeb/";//by yang 寝室
-    //public static final String url = "http://112.74.48.89:8080/SchoolTwoHandWeb/";//服务器地址
+    public static final String url = "http://112.74.48.89:8080/SchoolTwoHandWeb/";//服务器地址
 
-    public static final String imageUrl = "http://10.30.6.45:8080/image/";//by yang 高博
+    //public static final String imageUrl = "http://10.30.6.45:8080/image/";//by yang 高博
     //public static final String imageUrl = "http://192.168.191.1:8080/image/";//by yang 寝室
-    //public static final String imageUrl = "http://112.74.48.89:8080/imageLoad/";//服务器地址
+    public static final String imageUrl = "http://112.74.48.89:8080/imageLoad/";//服务器地址
 
 
 }
